dd09fe756a638b30720e9c6de33dad93824aa9a5
Andrew Darqui <andrew.darqui@gmail.com>
2015-08-23 02:36:33 -0400

    cleanup

--- haskell_src/Web/TodoMVC/Servant.hs
+++ haskell_src/Web/TodoMVC/Servant.hs
@@ -2,5 +2,5 @@ module Web.TodoMVC.Servant (
   module A
 ) where
 
-import Web.TodoMVC.Servant.API as A
-import Web.TodoMVC.Servant.Shared as A
+import           Web.TodoMVC.Servant.API    as A
+import           Web.TodoMVC.Servant.Shared as A
